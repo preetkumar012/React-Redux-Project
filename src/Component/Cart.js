bb8aaf0a2a4db506d1f118a0748d0f5b6dffd439
preetkumar012 <pksiet84@gmail.com>
2023-02-18 10:48:17 +0530

    first commit

--- src/Component/Cart.js
+++ src/Component/Cart.js
@@ -1,30 +1,49 @@
 // import React, { useState,useEffect } from 'react';
+// import Skeleton from 'react-loading-skeleton';
 // import { useSelector } from 'react-redux';
 // function Cart() {
+//     const[product, setProduct]=useState([]);
+//     const[filter, setFilter]=useState(product);
+//     const[loading, setLoading]=useState(false);
+//     useEffect(()=>{
+//         const getProduct= async () =>{
+//             setLoading(true);
+//             const response=await fetch("https://fakestoreapi.com/products");
+//             setProduct(await response.clone().json())
+//             setFilter(await response.json());
+//             setLoading(false);
+
+//         }
+//         getProduct();
+//     },[])
+
+//     const Loading=()=>{
+//         return(
+//             <>
+//             loading......
+//             </>
+//         )
+//     }
+//     const Skeleton=()=>{
+//         return(
+//             <>
+//             <div className='col-md-4'>
+//                 {filter.map((product,id)=>{
+//                 <img src={product.image} alt={product.title}/>
+//                 })}
+//             </div>
+//             </>
+//         )
+//     }
 
 //     return (
 //         <>
            
 //             <div className='container'>
-//                 <h2>welcome to noida</h2>
-//                 {filter.map((product)=>{
-//                      <div className='row'>
-//                      <div className='col-md-4'>
-//                          <img src={product.image} alt={product.title} height="200px" width="180px" />
-//                      </div>
-//                      <div className='col-md-4'>
-//                          <h3>{product.title}</h3>
-//                          <p className='lead fw-bolder'>
-//                              {product.qty} X $ {product.price}=$
-//                              {product.qty * product.price}
-//                          </p>
-//                          <button className='btn btn-outline-dark me-3' onClick={() => handleButton(product)}><i class="fa fa-minus"></i></button>
-//                          <button className='btn btn-outline-dark me-3' onClick={() => handleButton(product)}><i class="fa fa-plus"></i></button>
-//                      </div>
-//                  </div>
-
-//                 })}
-               
+//              <h2>welcome to noida</h2>
+//                {
+//                 loading ? <Loading/>:<Skeleton/>
+//                }
 //             </div>
 
 //         </>
--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react';
-function Contact(){
-    return(
+import './Contact.css';
+
+function Contact() {
+    return (
         <>
-        <h3>welcome to contact page</h3>
+            <div className='container my-4 py-4'>
+                <div className='row my-3 py-3'>
+                    <h2 className='fw-bold text-uppercase text-center text-primary'>welcome to contact page</h2>
+                   
+                    <input type="text" placeholder='enter your name' required />
+                    
+                    <input type="text" placeholder='enter your email' required/>
+                    <input type="phone" placeholder='enter your phone' />
+                    <input type="address" placeholder='enter your address' />
+                    <input type="text" placeholder='enter your password' />
+                    <input type="text" placeholder='enter your confirm password' />
+                  
+                    
+                </div>
+                <button className='btn btn-outline-primary w-100 fs-3 '>submit  </button>
+            </div>
         </>
     )
 }
